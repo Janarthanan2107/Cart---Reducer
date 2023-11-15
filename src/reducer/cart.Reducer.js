007fe6af55dad613143fcea28b4b5719e14c9892
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-11-15 10:44:48 +0530

    Update cart.Reducer.js

--- src/reducer/cart.Reducer.js
+++ src/reducer/cart.Reducer.js
@@ -1,16 +1,5 @@
 // reducer
 const cartReducer = (state, action) => {
-    // if (action.type === "CLEAR_CART") {
-    //     // clearing the cart items
-    //     return { ...state, cartData: [] };
-    // } else if (action.type === "REMOVE_CART_ITEM") {
-    //     // clearing the single cart item using id
-    //     const remaining = state.cartData.filter((item) => item.id !== action.payload);
-    //     return { ...state, cartData: remaining }
-    // } 
-
-    // return { ...state };
-
     switch (action.type) {
         case "CLEAR_CART":
             return { ...state, cartData: [] };
